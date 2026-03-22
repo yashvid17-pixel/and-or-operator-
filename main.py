7a6baadeca7d782f2e3e58b1a416df0809fce18f
yashvid17-pixel <yashvid17@gmail.com>
2026-03-22 14:55:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+number_1=int(input("enter a number"))
+number_2=int(input("enter a number"))
+if number_1 >90 and number_2<90:
+    print("and operater sucess")
+elif number_1 >90 or number_2<90:
+    print("or operator sucess")
+else:
+    print("no sucess")
